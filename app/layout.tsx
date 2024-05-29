04b47be904b61e5805eba61e022b21b573a47259
xiaohuangdou <36787710+xiaohuangdou666@users.noreply.github.com>
2024-05-29 23:53:42 +0800

    Update layout.tsx default language

--- app/layout.tsx
+++ app/layout.tsx
@@ -33,7 +33,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }) {
   return (
-    <html lang="en">
+    <html lang="zh">
       <head>
         <meta name="config" content={JSON.stringify(getClientConfig())} />
         <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no" />
